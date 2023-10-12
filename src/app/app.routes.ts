8ccc4efd7d38607076aab210dc378928f2ae1734
Mike Hartington <mikehartington@gmail.com>
2023-10-12 19:58:45 -0400

    demo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  // { path: '', redirectTo: '/us/browse/pl.0ef59752c0cd457dbf1391f08cbd936f', pathMatch: 'full' },
-  { path: '', redirectTo: '/us/browse/12', pathMatch: 'full' },
+  // { path: '', redirectTo: '/pl.0ef59752c0cd457dbf1391f08cbd936f', pathMatch: 'full' },
+  { path: '', redirectTo: '/12', pathMatch: 'full' },
 
   {
-    path: 'us/browse/:id',
+    path: ':id',
     loadComponent: () =>
       import('./pages/browse/browse.component').then((m) => m.BrowseComponent),
   },
